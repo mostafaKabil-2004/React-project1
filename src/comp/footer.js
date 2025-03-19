be71ce73c8c7f8bb999cb7be114a046c0378ec83
Mostafa Kabil <kabilmostafa060@gmail.com>
2025-03-19 04:07:44 +0200

    make heade.js & footer.js as a componenet to use it in all pages

---
+++ src/comp/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        Designed and Devoloped By Web devs
+        <span>🧡</span>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
